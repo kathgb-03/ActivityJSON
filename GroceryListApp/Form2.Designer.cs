0645362770e317d4a8736bc08d443047a8d65dd2
Katherine Grace <missmatchaa11@gmail.com>
2025-05-08 21:02:21 +0800

    .

--- GroceryListApp/Form2.Designer.cs
+++ GroceryListApp/Form2.Designer.cs
@@ -25,50 +25,36 @@
             this.btnSave = new System.Windows.Forms.Button();
             this.lblStatus = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // txtId
-            // 
+
             this.txtId.Location = new System.Drawing.Point(30, 20);
             this.txtId.Name = "txtId";
             this.txtId.Size = new System.Drawing.Size(220, 22);
-            // 
-            // txtName
-            // 
+
             this.txtName.Location = new System.Drawing.Point(30, 50);
             this.txtName.Name = "txtName";
             this.txtName.Size = new System.Drawing.Size(220, 22);
-            // 
-            // txtPrice
-            // 
+
             this.txtPrice.Location = new System.Drawing.Point(30, 80);
             this.txtPrice.Name = "txtPrice";
             this.txtPrice.Size = new System.Drawing.Size(220, 22);
-            // 
-            // btnAddToList
-            // 
+
             this.btnAddToList.Location = new System.Drawing.Point(30, 110);
             this.btnAddToList.Name = "btnAddToList";
             this.btnAddToList.Size = new System.Drawing.Size(100, 30);
             this.btnAddToList.Text = "Add to List";
             this.btnAddToList.Click += new System.EventHandler(this.btnAddToList_Click);
-            // 
-            // btnSave
-            // 
+
             this.btnSave.Location = new System.Drawing.Point(150, 110);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(100, 30);
             this.btnSave.Text = "Save";
             this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
-            // 
-            // lblStatus
-            // 
+
             this.lblStatus.Location = new System.Drawing.Point(30, 150);
             this.lblStatus.Name = "lblStatus";
             this.lblStatus.Size = new System.Drawing.Size(220, 30);
             this.lblStatus.Text = "Status here";
-            // 
-            // Form2
-            // 
+
             this.ClientSize = new System.Drawing.Size(300, 200);
             this.Controls.Add(this.txtId);
             this.Controls.Add(this.txtName);
