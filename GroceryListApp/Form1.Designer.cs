21a9b7f4353fb23ae861363ee3ac0126ad3ecc0a
Katherine Grace <missmatchaa11@gmail.com>
2025-05-08 21:01:04 +0800

    .

--- GroceryListApp/Form1.Designer.cs
+++ GroceryListApp/Form1.Designer.cs
@@ -19,33 +19,25 @@
             this.lstDisplay = new System.Windows.Forms.ListBox();
             this.btnOpenForm2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // btnLoadJson
-            // 
+
             this.btnLoadJson.Location = new System.Drawing.Point(30, 20);
             this.btnLoadJson.Name = "btnLoadJson";
             this.btnLoadJson.Size = new System.Drawing.Size(140, 30);
             this.btnLoadJson.Text = "Load Grocery List";
             this.btnLoadJson.UseVisualStyleBackColor = true;
             this.btnLoadJson.Click += new System.EventHandler(this.btnLoadJson_Click);
-            // 
-            // lstDisplay
-            // 
+
             this.lstDisplay.FormattingEnabled = true;
             this.lstDisplay.ItemHeight = 16;
             this.lstDisplay.Location = new System.Drawing.Point(30, 60);
             this.lstDisplay.Size = new System.Drawing.Size(300, 180);
-            // 
-            // btnOpenForm2
-            // 
+
             this.btnOpenForm2.Location = new System.Drawing.Point(190, 20);
             this.btnOpenForm2.Size = new System.Drawing.Size(140, 30);
             this.btnOpenForm2.Text = "Add Groceries";
             this.btnOpenForm2.UseVisualStyleBackColor = true;
             this.btnOpenForm2.Click += new System.EventHandler(this.btnOpenForm2_Click);
-            // 
-            // Form1
-            // 
+
             this.ClientSize = new System.Drawing.Size(380, 260);
             this.Controls.Add(this.btnLoadJson);
             this.Controls.Add(this.btnOpenForm2);
